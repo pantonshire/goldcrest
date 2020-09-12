f9870e86b4b2f5a50e02b7588ba8c82491f03646
Pantonshire <tom@tomandtally.co.uk>
2020-09-12 12:39:30 +0100

    Test new oauth functions

--- cmd/test/twapi/twapi.go
+++ cmd/test/twapi/twapi.go
@@ -2,71 +2,50 @@ package main
 
 import (
   "bufio"
-  "bytes"
-  "crypto/hmac"
-  "crypto/rand"
-  "crypto/sha1"
-  "encoding/base64"
   "fmt"
-  "github.com/martinlindhe/base36"
+  "goldcrest/twitter1"
   "io/ioutil"
   "net/http"
-  "net/url"
   "os"
   "path"
-  "sort"
   "strings"
   "time"
 )
 
 func main() {
-  //v1()
+  v1()
   //v2()
-  oauthParams := url.Values{}
-  //These are example keys and tokens, not real!
-  oauthParams.Set("oauth_consumer_key", "xvz1evFS4wEEPTGEFPHBog")
-  oauthParams.Set("oauth_nonce", "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg")
-  oauthParams.Set("oauth_signature_method", "HMAC-SHA1")
-  oauthParams.Set("oauth_timestamp", "1318622958")
-  oauthParams.Set("oauth_token", "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb")
-  oauthParams.Set("oauth_version", "1.0")
-
-  queryParams := url.Values{}
-  queryParams.Set("include_entities", "true")
-
-  bodyParams := url.Values{}
-  bodyParams.Set("status", "Hello Ladies + Gentlemen, a signed OAuth request!")
-
-  fmt.Println(getOAuthSignature(
-    //Again, example key and token
-    "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw",
-    "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE",
-    "POST",
-    "https://api.twitter.com/1.1/statuses/update.json",
-    oauthParams,
-    queryParams,
-    bodyParams,
-  ))
+  //oauthParams := twitter1.PercentEncodedParams{}
+  ////These are example keys and tokens, not real!
+  //oauthParams.Set("oauth_consumer_key", "xvz1evFS4wEEPTGEFPHBog")
+  //oauthParams.Set("oauth_nonce", "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg")
+  //oauthParams.Set("oauth_signature_method", "HMAC-SHA1")
+  //oauthParams.Set("oauth_timestamp", "1318622958")
+  //oauthParams.Set("oauth_token", "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb")
+  //oauthParams.Set("oauth_version", "1.0")
+  //
+  //queryParams := twitter1.PercentEncodedParams{}
+  //queryParams.Set("include_entities", "true")
+  //
+  //bodyParams := twitter1.PercentEncodedParams{}
+  //bodyParams.Set("status", "Hello Ladies + Gentlemen, a signed OAuth request!")
+  //
+  //fmt.Println(getOAuthSignature(
+  //  //Again, example key and token
+  //  "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw",
+  //  "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE",
+  //  "POST",
+  //  "https://api.twitter.com/1.1/statuses/update.json",
+  //  oauthParams,
+  //  queryParams,
+  //  bodyParams,
+  //))
 }
 
 func v1() {
   client := http.Client{
     Timeout: time.Second * 5,
   }
-
-  version := "1.1"
-  method := "POST"
-  reqPath := "statuses/update.json"
-  baseURL := path.Join("https://api.twitter.com", version, reqPath)
-  parameters := map[string]string{
-    "status": "Hello, world!",
-  }
-
-  vals := url.Values{}
-  for key, value := range parameters {
-    vals.Set(key, value)
-  }
-  paramStr := vals.Encode() //TODO: use percentEncode
 
   reader := bufio.NewReader(os.Stdin)
 
@@ -76,60 +55,18 @@ func v1() {
     readLn(reader, "access token"),
     readLn(reader, "token secret")
 
-  oauthVals := url.Values{}
-  oauthVals.Set("oauth_consumer_key", consumerKey)
-  oauthVals.Set("oauth_token", token)
-  oauthVals.Set("oauth_signature_method", "HMAC-SHA1")
-  oauthVals.Set("oauth_version", "1.0")
-  oauthVals.Set("oauth_timestamp", fmt.Sprintf("%d", time.Now().Unix()))
-
-  randBytes := make([]byte, 32)
-  _, err := rand.Read(randBytes)
+  req, err := twitter1.OAuthRequest{
+    Method:   "POST",
+    Protocol: "https",
+    Domain:   "api.twitter.com",
+    Path:     path.Join("1.1", "statuses/update.json"),
+    Body: map[string]string{
+      "status": "One final hello world, probably 😎",
+    },
+  }.MakeRequest(twitter1.Auth{Key: secretKey, Token: tokenSecret}, twitter1.Auth{Key: consumerKey, Token: token})
   if err != nil {
     panic(err)
   }
-  oauthVals.Set("oauth_nonce", base36.EncodeBytes(randBytes))
-
-  signature := getOAuthSignature(secretKey, tokenSecret, method, baseURL, oauthVals, nil, vals)
-  oauthVals.Set("oauth_signature", signature)
-
-  var dstBuilder strings.Builder
-  dstBuilder.WriteString("OAuth ")
-  {
-    keys := make([]string, len(oauthVals))
-    var i int
-    for k := range oauthVals {
-      keys[i] = k
-      i++
-    }
-    sort.Strings(keys)
-    var writtenFirst bool
-    for _, key := range keys {
-      vals := oauthVals[key]
-      escapedKey := percentEncode(key)
-      for _, val := range vals {
-        if writtenFirst {
-          dstBuilder.WriteString(", ")
-        } else {
-          writtenFirst = true
-        }
-        dstBuilder.WriteString(escapedKey)
-        dstBuilder.WriteRune('=')
-        dstBuilder.WriteRune('"')
-        dstBuilder.WriteString(percentEncode(val))
-        dstBuilder.WriteRune('"')
-      }
-    }
-  }
-  dst := dstBuilder.String()
-
-  req, err := http.NewRequest(method, baseURL, bytes.NewBufferString(paramStr))
-  if err != nil {
-    panic(err)
-  }
-
-  req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-  req.Header.Set("Authorization", dst)
 
   resp, err := client.Do(req)
   if err != nil {
@@ -191,52 +128,6 @@ func v2() {
   fmt.Println(string(respBody))
 }
 
-func getOAuthSignature(consumerSecret, tokenSecret, method, baseURL string, oauthParams, queryParams, bodyParams map[string][]string) string {
-  allParams := make(map[string][]string)
-  for _, params := range []url.Values{oauthParams, queryParams, bodyParams} {
-    for key, values := range params {
-      for _, value := range values {
-        allParams[key] = append(allParams[key], value)
-      }
-    }
-  }
-
-  var paramBuilder strings.Builder
-  {
-    keys := make([]string, len(allParams))
-    var i int
-    for k := range allParams {
-      keys[i] = k
-      i++
-    }
-    sort.Strings(keys)
-    var writtenFirst bool
-    for _, key := range keys {
-      vals := allParams[key]
-      escapedKey := percentEncode(key)
-      for _, val := range vals {
-        if writtenFirst {
-          paramBuilder.WriteString("&")
-        } else {
-          writtenFirst = true
-        }
-        paramBuilder.WriteString(escapedKey)
-        paramBuilder.WriteRune('=')
-        paramBuilder.WriteString(percentEncode(val))
-      }
-    }
-  }
-  paramStr := paramBuilder.String()
-
-  sigBase := strings.ToUpper(method) + "&" + percentEncode(baseURL) + "&" + percentEncode(paramStr)
-  signingKey := percentEncode(consumerSecret) + "&" + percentEncode(tokenSecret)
-
-  h := hmac.New(sha1.New, []byte(signingKey))
-  h.Write([]byte(sigBase))
-
-  return base64.StdEncoding.EncodeToString(h.Sum(nil))
-}
-
 func readLn(reader *bufio.Reader, prompt string) string {
   fmt.Print(fmt.Sprintf("%s :> ", prompt))
   str, err := reader.ReadString('\n')
@@ -244,32 +135,4 @@ func readLn(reader *bufio.Reader, prompt string) string {
     panic(err)
   }
   return strings.TrimSpace(str)
-}
-
-func percentEncode(raw string) string {
-  var dst []byte
-  for _, b := range []byte(raw) {
-    if skipPercentageEncode(b) {
-      dst = append(dst, b)
-    } else {
-      b1, b2 := toHex(b)
-      dst = append(dst, 0x25, b1, b2)
-    }
-  }
-  return string(dst)
-}
-
-func skipPercentageEncode(r byte) bool {
-  return (0x30 <= r && r <= 0x39) || (0x41 <= r && r <= 0x5A) || (0x61 <= r && r <= 0x7A) ||
-    r == 0x2D || r == 0x2E || r == 0x5F || r == 0x7E
-}
-
-func toHex(b byte) (b1, b2 byte) {
-  if b1 = (b >> 4) + 0x30; b1 > 0x39 {
-    b1 += 0x7
-  }
-  if b2 = (b & 0xF) + 0x30; b2 > 0x39 {
-    b2 += 0x7
-  }
-  return b1, b2
 }
