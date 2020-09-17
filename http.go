325918f87365f4527c88828cde35eefaea612e69
Pantonshire <tom@tomandtally.co.uk>
2020-09-17 20:56:16 +0100

    HttpError functions now return error interface so that returned nil values are untyped

--- http.go
+++ http.go
@@ -15,14 +15,14 @@ func (e *HttpError) Error() string {
   return fmt.Sprintf("%d %s", e.Code, e.Status)
 }
 
-func NewHttpError(code int, status string) *HttpError {
+func NewHttpError(code int, status string) error {
   if IsStatusOK(code) {
     return nil
   }
   return &HttpError{Code: code, Status: status}
 }
 
-func HttpErrorFor(resp *http.Response) *HttpError {
+func HttpErrorFor(resp *http.Response) error {
   var status string
   if splitStatus := strings.SplitN(resp.Status, " ", 2); len(splitStatus) > 1 {
     status = splitStatus[1]
