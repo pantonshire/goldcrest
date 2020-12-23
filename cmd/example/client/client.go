f7324f33e7489d33af65ecd8bb365e98a321a53c
Pantonshire <tom@tomandtally.co.uk>
2020-12-23 17:03:20 +0000

    Update client example

--- cmd/example/client/client.go
+++ cmd/example/client/client.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-  "context"
   "encoding/json"
   "github.com/davecgh/go-spew/spew"
-  pb "github.com/pantonshire/goldcrest/protocol"
+  "github.com/pantonshire/goldcrest/client/go/au"
   "github.com/pantonshire/goldcrest/proxy/oauth"
   "google.golang.org/grpc"
   "io/ioutil"
@@ -21,12 +20,12 @@ func main() {
     panic(err)
   }
 
-  authMsg := pb.Authentication{
-    ConsumerKey: auth.Public.Key,
-    AccessToken: auth.Public.Token,
-    SecretKey:   auth.Secret.Key,
-    SecretToken: auth.Secret.Token,
-  }
+  //authMsg := pb.Authentication{
+  //  ConsumerKey: auth.Public.Key,
+  //  AccessToken: auth.Public.Token,
+  //  SecretKey:   auth.Secret.Key,
+  //  SecretToken: auth.Secret.Token,
+  //}
 
   conn, err := grpc.Dial("localhost:7400", grpc.WithBlock(), grpc.WithInsecure())
   if err != nil {
@@ -34,18 +33,76 @@ func main() {
   }
   defer conn.Close()
 
-  client := pb.NewTwitter1Client(conn)
+  client := au.NewClient(conn).
+    WithAuth(auth.Public.Key, auth.Public.Token, auth.Secret.Key, auth.Secret.Token).
+    WithTimeout(time.Second * 5).
+    WithRetryLimit(5)
 
-  ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-  defer cancel()
-
-  tweet, err := client.GetTweet(ctx, &pb.TweetRequest{
-    Auth: &authMsg,
-    Id:   1339937761126658049,
-  })
+  //resp, err := client.GetTweet(1224097454477512704)
+  //resp, err := client.RetweetTweet(1309868095306162176)
+  resp, err := client.UserTimeline(au.UserHandle("smolrobots"), au.NewTimelineOptions(5), true, true)
   if err != nil {
     panic(err)
   }
 
-  spew.Dump(tweet)
+  spew.Dump(resp)
+
+  //client := pb.NewTwitterClient(conn)
+  //
+  //ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+  //defer cancel()
+
+  //resp, err := client.GetTweets(ctx, &pb.TweetsRequest{
+  //  Auth: &authMsg,
+  //  Ids:  []uint64{1339937761126658049, 1250097187377360896, 1339877652929245184, 1074568259385597952},
+  //})
+
+  //resp, err := client.PublishTweet(ctx, &pb.PublishTweetRequest{
+  // Auth:                      &authMsg,
+  // Text:                      "I think I messed up my rate limit tracker?",
+  // AutoPopulateReplyMetadata: false,
+  // PossiblySensitive:         false,
+  //})
+
+  //resp, err := client.RetweetTweet(ctx, &pb.TweetRequest{
+  // Auth: &authMsg,
+  // Id:   1340711006440480768,
+  //})
+
+  //resp, err := client.GetUserTimeline(ctx, &pb.UserTimelineRequest{
+  //  Auth: &authMsg,
+  //  User: &pb.UserTimelineRequest_UserHandle{UserHandle: "PantonshireDev"},
+  //  TimelineOptions: &pb.TimelineOptions{
+  //    Count: 15,
+  //    Content: &pb.TimelineOptions_Custom{
+  //      Custom: &pb.TweetOptions{
+  //        TrimUser:          true,
+  //        IncludeMyRetweet:  true,
+  //        IncludeEntities:   true,
+  //        IncludeExtAltText: true,
+  //        IncludeCardUri:    true,
+  //        Mode:              pb.TweetOptions_EXTENDED,
+  //      },
+  //    },
+  //  },
+  //  IncludeReplies:  true,
+  //  IncludeRetweets: true,
+  //})
+
+  //resp, err := client.UpdateProfile(ctx, &pb.UpdateProfileRequest{
+  //  Auth:           &authMsg,
+  //  UpdateLocation: &pb.UpdateProfileRequest_Location{Location: "Testing with Goldcrest v0.1"},
+  //  UpdateBio:      &pb.UpdateProfileRequest_Bio{Bio: "Test account for @smolbotbot!"},
+  //  UpdateName:     &pb.UpdateProfileRequest_Name{Name: "Smolbotbot Test"},
+  //})
+  //
+  //if err != nil {
+  //  panic(err)
+  //}
+  //
+  //s, err := json.MarshalIndent(resp, "", " ")
+  //if err != nil {
+  //  panic(err)
+  //}
+  //fmt.Println(string(s))
 }
